03ec00659de02e0e8bddf0d4c3c1db131bfee67c
Manghui <52086853+Manghui@users.noreply.github.com>
2023-02-02 14:00:15 +0800

    Update

---
+++ src/Config.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ServerQueryer
+{
+	public class Config
+    {
+        public bool is_enable { get; private set; } = true;
+    }
+}
---
+++ src/EntryPoint.cs
@@ -0,0 +1,28 @@
+﻿using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+using PluginAPI.Events;
+
+namespace ServerQueryer
+{
+
+    public class EntryPoint
+    {
+        public static EntryPoint Singleton { get; private set; }
+
+        [PluginPriority(LoadPriority.Highest)]
+        [PluginEntryPoint("ServerQueryer", "1.0.0", "插件介绍", "Manghui")]
+        void LoadPlugin()
+        {
+            Singleton = this;
+            if (!PluginConfig.is_enable)
+                return;
+            Log.Info("Loaded plugin, register events...");
+            EventManager.RegisterEvents<EventHandlers>(this);
+            Log.Info($"Plugin Loaded.");
+        }
+
+        [PluginConfig] public Config PluginConfig;
+
+    }
+}
